a2cbce8eeb537f43dcd6abc6dc850a532b3b76a0
dag18e <dag18e@my.fsu.edu>
2020-05-20 00:46:14 -0500

    Add: New simplified GUI with scrollable status box

--- GUI.java
+++ GUI.java
@@ -1,40 +1,51 @@
-import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 
 public class GUI extends JFrame implements ActionListener {
     
-    private static JLabel status;
+    private static JTextArea status;
+    private static LocalTime time;
+    private static DateTimeFormatter timeFormat;
 
     public GUI() {
-        super("classNabber");
-        BorderLayout layout = new BorderLayout();
+    	super("classNabber");
+        GridBagLayout layout = new GridBagLayout();
+        GridBagConstraints con = new GridBagConstraints();
         setLayout(layout);
 
-
-        //set up North bar
-        JPanel northBar = new JPanel();
-        JButton add = new JButton("add");
-        JButton remove = new JButton("remove");
-        add(northBar, BorderLayout.NORTH);
-        northBar.add(add);
-        northBar.add(remove);
+        JButton run = new JButton("Run");
+        JButton stop = new JButton("Stop");
+        con.anchor = GridBagConstraints.NORTH;
+        con.fill = GridBagConstraints.BOTH;
+        con.weightx = 50;
+        con.weighty = 10;
+        con.gridx = 0;
+        con.gridy = 0;
+        add(run, con);
+        con.gridx = 1;
+        add(stop, con);
         
-        //set up South bar
-        JPanel southBar = new JPanel();
-        status = new JLabel("Status: Starting...");
-        add(southBar, BorderLayout.SOUTH);
-        southBar.add(status);
-
-        //set up class list
-        JPanel classList = new JPanel();
-        // if(!userClasses.isEmpty()) {
-            // for(int i = 0; i < userClasses.size(); i++) {
-            //     JCheckBox classCheckBoxes[i] = new JCheckbox(userClasses.get(i));
-            // }
-        // }
+        
+        timeFormat = DateTimeFormatter.ofPattern("HH:mm:ss");
+        time = LocalTime.now();
+        
+        
+        status = new JTextArea("[" + time.format(timeFormat) + "]: Enabling hyperdrive...");
+        status.setEditable(false);
+        status.setLineWrap(true);
+        JScrollPane statusBox = new JScrollPane(status);
+        
+        con.weighty = 80;
+        con.gridx = 0;
+        con.gridy = 1;
+        con.gridwidth = 2;
+        add(statusBox, con);
     }
 
     @Override
@@ -43,7 +54,11 @@ public class GUI extends JFrame implements ActionListener {
     }
 
     public void setStatus(String s) {
-        status.setText("Status: " + s);
+    	time = LocalTime.now();
+    	time.format(timeFormat);
+    	
+        status.append("\n[" + time.format(timeFormat) + "]: " + s);
+        status.setCaretPosition(status.getDocument().getLength());
         return;
     }
 }
